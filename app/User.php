943f218ff54e670c0e03da5c08b24ed98e40cbfb
Luis Mayta <luis.mayta@gmail.com>
2018-09-01 00:46:11 -0500

    Login

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    /**
+     * Atributos de la clase Users
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+
+
 }
